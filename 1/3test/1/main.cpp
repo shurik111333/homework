66fb8f0b967fa52062c9bc9fc601f92847d15f10
Aleksandr Chudov <chudov.aleksandr94@gmail.com>
2015-12-18 15:12:57 +0300

    Optimized problem 1

--- 1/3test/1/main.cpp
+++ 1/3test/1/main.cpp
@@ -8,23 +8,17 @@ const int maxLen = 256;
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	cout << "¬ведите довичную запись целого числа" << endl;
+	cout << "¬ведите двоичную запись целого положительного числа" << endl;
 	char number[maxLen] = {};
 	cin >> number;
 	int len = strlen(number);
-	int *powerOfTwo = new int[len];
-	powerOfTwo[0] = 1;
-	for (int i = 1; i < len; i++)
-	{
-		powerOfTwo[i] = powerOfTwo[i - 1] * 2;
-	}
 	int result = 0;
+	int currentPower = 1;
 	for (int i = len - 1; i >= 0; i--)
 	{
-		if (number[i] == '1')
-			result += powerOfTwo[len - i - 1];
+		result += (number[i] - '0') * currentPower;
+		currentPower <<= 1;
 	}
-	delete[] powerOfTwo;
 	cout << "ƒес€тична€ запись числа: " << result << endl;
 	return 0;
 }
