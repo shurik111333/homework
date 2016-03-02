ca3fc007cd0de22660c599a3c3c46eae7bed83b5
Aleksandr Chudov <chudov.aleksandr94@gmail.com>
2016-03-02 10:12:35 +0300

    Add problem 3.2

---
+++ 2/3hw/2/MainWindow.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QObject>
+#include <QMainWindow>
+
+namespace Ui {
+	class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+	
+public:
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
+	
+private:
+	Ui::MainWindow *ui;
+	
+public slots:
+	void calculate();
+};
+
