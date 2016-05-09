9e95cf20dd0e1e823401d16021c3da54b4123d2e
Aleksandr Chudov <chudov.aleksandr94@gmail.com>
2016-05-09 14:21:28 +0300

    Add 7.2

---
+++ 2/7hw/2/vector.h
@@ -0,0 +1,141 @@
+#pragma once
+
+#include <stdexcept>
+
+using std::out_of_range;
+
+template <int n>
+class Vector
+{
+public:
+	/**
+	 * @brief Create vector, in which all coordinates are equal value
+	 * @param value
+	 */
+	Vector(const int value = 0);
+	/**
+	 * @brief Copies first n elements from input array as coordinates
+	 * @param v
+	 */
+	Vector(const int *v);
+	Vector(const Vector<n> &v);
+	~Vector();
+
+	bool isNull() const noexcept;
+	/**
+	 * @brief at
+	 * @param i
+	 * @return Return i-th coordinate, i must be form 0 till n - 1
+	 */
+	int at(const int i) const throw(out_of_range);
+	/**
+	 * @brief operator []
+	 * @param i
+	 * @return Return link to i-th coordinate, i must be form 0 till n - 1
+	 */
+	int &operator [] (const int i) throw(out_of_range);
+	bool operator == (const Vector<n> &v) const noexcept;
+	Vector<n> operator - (const Vector<n> &v) const noexcept;
+	Vector<n> operator + (const Vector<n> &v) const noexcept;
+	/**
+	 * @brief operator * Scalar product of two n-dimensional vectors
+	 * @param v
+	 * @return
+	 */
+	int operator * (const Vector<n> &v) const noexcept;
+
+private:
+	int vector[n];
+};
+
+template <int n>
+Vector<n>::Vector(const int value)
+{
+	for (int i = 0; i < n; i++)
+		vector[i] = value;
+}
+
+template <int n>
+Vector<n>::Vector(const int *v)
+{
+	for (int i = 0; i < n; i++)
+		vector[i] = v[i];
+}
+
+template <int n>
+Vector<n>::Vector(const Vector<n> &v)
+{
+	for (int i = 0; i < n; i++)
+		vector[i] = v.at(i);
+}
+
+template <int n>
+Vector<n>::~Vector()
+{
+	delete[] vector;
+}
+
+template <int n>
+bool Vector<n>::isNull() const noexcept
+{
+	bool res = true;
+	for (int i = 0; i < n; i++)
+		res &= (vector[i] == 0);
+	return res;
+}
+
+template <int n>
+int Vector<n>::at(const int i) const throw(out_of_range)
+{
+	if (i < 0 || i >= n)
+		throw out_of_range("Coordinate with index " +
+	                       std::to_string(i) +
+	                       " does not exist in vector");
+	return vector[i];
+}
+
+template <int n>
+int &Vector<n>::operator [](const int i) throw(out_of_range)
+{
+	if (i < 0 || i >= n)
+		throw out_of_range("Coordinate with index " +
+	                       std::to_string(i) +
+	                       " does not exist in vector");
+	return vector[i];
+}
+
+template <int n>
+bool Vector<n>::operator == (const Vector<n> &v) const noexcept
+{
+	bool res = true;
+	for (int i = 0; i < n; i++)
+		res &= (this->at(i) == v.at(i));
+	return res;
+}
+
+template <int n>
+Vector<n> Vector<n>::operator - (const Vector<n> &v) const noexcept
+{
+	int res[n];
+	for (int i = 0; i < n; i++)
+		res[i] = this->at(i) - v.at(i);
+	return Vector<n>(res);
+}
+
+template <int n>
+Vector<n> Vector<n>::operator + (const Vector<n> &v) const noexcept
+{
+	int res[n];
+	for (int i = 0; i < n; i++)
+		res[i] = this->at(i) + v.at(i);
+	return Vector<n>(res);
+}
+
+template <int n>
+int Vector<n>::operator * (const Vector<n> &v) const noexcept
+{
+	int res = 0;
+	for (int i = 0; i < n; i++)
+		res += this->at(i) * v.at(i);
+	return res;
+}
---
+++ 2/7hw/2/vectortest.h
@@ -0,0 +1,123 @@
+#pragma once
+
+#include <QtCore/QObject>
+#include <QtTest/QtTest>
+#include <cstdlib>
+#include <ctime>
+#include "vector.h"
+
+class VectorTest : public QObject
+{
+	Q_OBJECT
+
+public:
+	explicit VectorTest(QObject *parent = 0):
+	    QObject(parent)
+	{
+		srand(time(0));
+	}
+
+private:
+	void fillCoordinates(int *coords, int n)
+	{
+		for (int i = 0; i < n; i++)
+			coords[i] = rand() % range - range / 2;
+	}
+
+	const int range = 200;
+
+private slots:
+	void testCreateNullVector()
+	{
+		Vector<5> v;
+		QVERIFY(v.isNull());
+	}
+
+	void testCreateVectorWithEqualCoordinates()
+	{
+		Vector<5> v1(1);
+		Vector<5> v2(7);
+		for (int i = 0; i < 0; i++)
+		{
+			QCOMPARE(v1[i], 1);
+			QCOMPARE(v2[i], 7);
+		}
+	}
+
+	void testCreateVectorWithDifferentCoordinates()
+	{
+		int coords1[6];
+		int coords2[6];
+		fillCoordinates(coords1, 6);
+		fillCoordinates(coords2, 6);
+		Vector<6> v1(coords1);
+		Vector<6> v2(coords2);
+		for (int i = 0; i < 6; i++)
+		{
+			QCOMPARE(v1[i], coords1[i]);
+			QCOMPARE(v2[i], coords2[i]);
+		}
+	}
+
+	void testEqualOperator()
+	{
+		int coords1[6];
+		int coords2[6];
+		fillCoordinates(coords1, 6);
+		fillCoordinates(coords2, 6);
+		coords2[0] = coords1[0] + 1; //for exclude equal coordinates
+		Vector<6> v1(coords1);
+		Vector<6> v2(coords1);
+		QVERIFY(v1 == v2);
+		Vector<6> v3(coords2);
+		QVERIFY(!(v1 == v3));
+	}
+
+	void testCopyConstructor()
+	{
+		int coords[6];
+		fillCoordinates(coords, 6);
+		Vector<6> v1(coords);
+		Vector<6> v2(v1);
+		QVERIFY(v1 == v2);
+	}
+
+	void testSubtractAndAdditionVectors()
+	{
+		int coords1[6];
+		int coords2[6];
+		for (int i = 0; i < 100; i++)
+		{
+			fillCoordinates(coords1, 6);
+			fillCoordinates(coords2, 6);
+			Vector<6> v1(coords1);
+			Vector<6> v2(coords2);
+			auto v3 = v1 - v2;
+			auto v4 = v1 + v2;
+			for (int j = 0; j < 6; j++)
+			{
+				QCOMPARE(v3[j], (coords1[j] - coords2[j]));
+				QCOMPARE(v4[j], (coords1[j] + coords2[j]));
+			}
+		}
+	}
+
+	void testScalarProduct()
+	{
+		int coords1[6];
+		int coords2[6];
+		for (int i = 0; i < 100; i++)
+		{
+			fillCoordinates(coords1, 6);
+			fillCoordinates(coords2, 6);
+			Vector<6> v1(coords1);
+			Vector<6> v2(coords2);
+			int res = 0;
+			for (int j = 0; j < 6; j++)
+			{
+				res += coords1[j] * coords2[j];
+			}
+			QCOMPARE(v1 * v2, res);
+		}
+	}
+};
