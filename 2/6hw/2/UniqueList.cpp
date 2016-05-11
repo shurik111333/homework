9832ab18e88dafa08548987e825411f47d76c778
Aleksandr Chudov <chudov.aleksandr94@gmail.com>
2016-05-11 20:13:37 +0300

    Fixed UniqueList::insert()

--- 2/6hw/2/UniqueList.cpp
+++ 2/6hw/2/UniqueList.cpp
@@ -6,14 +6,7 @@ UniqueList::UniqueList():
 
 void UniqueList::insert(int value, int index) throw(ElementAlreadyExistException)
 {
-	if (count == size)
-		increaseList();
 	if (findElementId(value) != -1)
 		throw ElementAlreadyExistException();
-	for (int i = count; i > index; i--)
-	{
-		array[i] = array[i - 1];
-	}
-	array[index] = value;
-	count++;
+	ListArray::insert(value, index);
 }
