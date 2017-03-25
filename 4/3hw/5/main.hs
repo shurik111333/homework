b2245a2ae58ac291626b2fab86c107d74fcb16c5
Aleksandr Chudov <chudov.aleksandr94@gmail.com>
2017-03-25 11:34:17 +0300

    Fix for negative numbers

--- 4/3hw/5/main.hs
+++ 4/3hw/5/main.hs
@@ -1,8 +1,7 @@
 fib :: Int -> Int
-fib n = fib' n 1 1
-	where
-		fib' :: Int -> Int -> Int -> Int
-		fib' n _ _ | n < 0 = 0
-		fib' 0 a _ = a
-		fib' 1 _ b = b
-		fib' n a b = fib' (n - 1) b (a + b)
+fib n = fib' n 0 1
+    where
+        fib' :: Int -> Int -> Int -> Int
+        fib' 0 a _ = a
+        fib' n a b | n > 0 = fib' (n - 1) b (a + b)
+        fib' n a b | n < 0 = fib' (n + 1) b (a - b)
